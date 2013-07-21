6d3d92c407493507abb93622cf62685468cdb74b
Michael Chamkin <mfixed@gmail.com>
2013-07-21 06:18:16 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,22 +1,15 @@
-var fs = require('fs');
-
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-var filename = "index.html";
-
-var mybuffer = new Buffer('utf8');
-var mystring;
-
-mybuffer = new Buffer(fs.readFileSync(filename));
-mystring = mybuffer.toString();
-
 app.get('/', function(request, response) {
-  response.send(mystring);
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
